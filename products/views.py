72344c4cfd4a6ef9dfeeea458c32fecec3398441
Rafael Guibson <rafaelguibsons@gmail.com>
2025-02-13 16:18:22 -0300

    Adjusted products_view to receive all produts and pass to the template trough context

--- products/views.py
+++ products/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+# Importar o model para chamar a listagem de produtos
+from products.models import Product
+
 # Create your views here.
 def products_view(request):
-    return HttpResponse('<h1>Hello, World!</h1>')
+    # Lista Todos os produtos, como uma query select * from produts
+    products_list = Product.objects.all()
+    return render(
+        request=request, 
+        template_name='products.html',
+        context={'products_list': products_list})
