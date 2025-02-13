87bc38fd2fe4ae9ed4f861681d824fbe210b083d
Rafael Guibson <rafaelguibsons@gmail.com>
2025-02-13 15:41:25 -0300

    Added products view and path for products

--- app/urls.py
+++ app/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from products.views import products_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('products/', products_view)
 ]
--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+class ProductAdmin(admin.ModelAdmin):
+    # Colunas que serão exibidas no modulo admin
+    list_display = ('title', 'category', 'brand', 'price', )
+    # Campos que serão considerados para busca
+    search_fields = ('title',)
+    # Campos que serão considerados para filtros.
+    list_filter = ('category', 'brand',)
+
+# Registrar o product admin com o model de product
+admin.site.register(models.Product, ProductAdmin)
